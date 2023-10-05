e98fd99e218db0e1b686bb87bdd6c49f0a32e1fd
OAsat <o-asa@g.ecc.u-tokyo.ac.jp>
2023-10-05 12:29:18 +0900

    def_write

--- lib/labex/instrument/model.ex
+++ lib/labex/instrument/model.ex
@@ -12,8 +12,18 @@ defmodule Labex.Instrument.Model do
       def read(unquote(key), params, impl_module) do
         unquote(query_format)
         |> Format.format_query(params)
-        |> impl_module.query(unquote(key))
+        |> impl_module.read(unquote(key))
         |> Format.parse_answer(unquote(answer_format))
+      end
+    end
+  end
+
+  defmacro def_write(key, query_format) do
+    quote do
+      def write(unquote(key), params, impl_module) do
+        unquote(query_format)
+        |> Format.format_query(params)
+        |> impl_module.write(unquote(key))
       end
     end
   end
--- test/model_test.exs
+++ test/model_test.exs
@@ -4,15 +4,21 @@ defmodule ModelTest do
 
   test "def model" do
     defmodule DummyInstrument do
-      def query("KRDG? A", _opts), do: "12.34"
+      alias Labex.Instrument.Impl
+      @behaviour Impl
+
+      def read("KRDG? A", _opts), do: "12.34"
+      def write("SETP A, 123.4", _opts), do: :ok
     end
 
     defmodule MyModel do
       use Labex.Instrument.Model
 
       def_read(:kelvin, "KRDG? ~s", "~f")
+      def_write(:setp, "SETP ~s, ~p")
     end
 
     assert MyModel.read(:kelvin, ["A"], DummyInstrument) == [12.34]
+    assert MyModel.write(:setp, ["A", 123.4], DummyInstrument) == :ok
   end
 end
