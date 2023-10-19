5ac00c024673b83d5e1c9c37eabf30519c09a621
OAsat <o-asa@g.ecc.u-tokyo.ac.jp>
2023-10-20 03:46:31 +0900

    mod format

--- lib/labex/format.ex
+++ lib/labex/format.ex
@@ -1,58 +1,90 @@
 defmodule Labex.Format do
-  @doc """
-  Formats a keyword list to string.
-      iex> Labex.Format.format("message:{key1},{key2},{key3},{key1}", [key1: "hello", key2: 12, key3: 3.4])
-      "message:hello,12,3.4,hello"
+  @moduledoc """
+  Modules for formatting and parsing messages.
   """
-  def format(format, param_list) do
-    Regex.replace(
-      ~r/\{(.+?)\}/,
-      format,
-      fn _, name -> get_from_keywordlist(param_list, name) |> to_string() end
-    )
-  end
+
+  @regex ~r/\{\{(.+?)\}\}/
 
   @doc """
   Parses string to a keyword list.
-      iex> Labex.Format.parse("message:hello,12,3.4", "message:{key1:str},{key2:int},{key3:float}")
+      iex> Labex.Format.parse("message:hello,12,3.4,end", "message:{{key1:str}},{{key2:int}},{{key3:float}},end")
       [key1: "hello", key2: 12, key3: 3.4]
   """
   def parse(str, format) do
-    {regex, keys, types} = expand_parse_format(format)
+    keys_and_types = extract_keys_and_types(format)
+    regex = String.replace(format, @regex, "(.+)")
     [_ | values] = Regex.run(~r/#{regex}/, str)
-
-    Enum.zip([values, keys, types])
-    |> Enum.map(fn {value, key, type} -> {key, parse_func(type).(value)} end)
+    Enum.zip(values, keys_and_types)
+    |> Enum.map(fn {value, {key, type}} -> {key, parse_func(type).(value)} end)
   end
 
-  defp expand_parse_format(format) do
-    {regex, keys, types} =
-      Regex.scan(~r/\{(.+?):(.+?)\}/, format)
-      |> Enum.reduce(
-        {format, [], []},
-        fn [pattern, key, type], {regex, keys, types} ->
-          {
-            Regex.replace(~r/#{pattern}/, regex, "(.+)"),
-            [String.to_atom(key) | keys],
-            [type | types]
-          }
-        end
-      )
+  @spec format(binary(), Keyword.t()) :: binary()
+  @doc """
+  Formats a keyword list to string.
 
-    {regex, Enum.reverse(keys), Enum.reverse(types)}
+      iex> Labex.Format.format("message:{{key1}},{{key2}},{{key3}}:end", key1: "hello", key2: 12, key3: 3.4)
+      "message:hello,12,3.4:end"
+      iex> Labex.Format.format("message")
+      "message"
+  """
+  def format(format, kw_list \\ []) do
+    format_peace = String.split(format, @regex)
+    keys = extract_keys_and_types(format) |> Keyword.keys()
+
+    out =
+      Enum.zip(format_peace, keys)
+      |> Enum.reduce("", fn {peace, key}, acc -> "#{acc}#{peace}#{Keyword.get(kw_list, key)}" end)
+
+    "#{out}#{List.last(format_peace)}"
   end
 
-  defp parse_func(type) do
-    case type do
-      "str" -> & &1
-      "float" -> &elem(Float.parse(&1), 0)
-      "int" -> &String.to_integer(&1)
+  @spec extract_keys_and_types(binary()) :: Keyword.t()
+  @doc """
+  Returns a keyword-list of keys:types from a format string.
+
+      iex> Labex.Format.extract_keys_and_types("message:{{key1:some_type}},{{key2}}")
+      [key1: :some_type, key2: nil]
+  """
+  def extract_keys_and_types(format) do
+    Regex.scan(@regex, format)
+    |> Enum.map(fn [_, pattern] -> split_key_and_type(pattern) end)
+  end
+
+  @spec split_key_and_type(binary()) :: {atom(), atom()}
+  @doc """
+  Splits a string to a key and a type.
+
+      iex> Labex.Format.split_key_and_type("key1:str")
+      {:key1, :str}
+      iex> Labex.Format.split_key_and_type("key1")
+      {:key1, nil}
+  """
+  def split_key_and_type(key) do
+    case String.split(key, ":") do
+      [name, type] -> {String.to_atom(name), String.to_atom(type)}
+      [name] -> {String.to_atom(name), nil}
     end
   end
 
-  defp get_from_keywordlist(keyword_list, name) do
-    # keyword-list to map
-    Enum.into(keyword_list, %{})
-    |> Map.get(String.to_atom(name))
+  @spec parse_func(:float | :int | :str) :: (binary() -> any())
+  @doc """
+  Returns a function that parses a string to a given type.
+
+      iex> Labex.Format.parse_func(:str).("hello")
+      "hello"
+      iex> Labex.Format.parse_func(:float).("12.34")
+      12.34
+      iex> Labex.Format.parse_func(:int).("1234")
+      1234
+      iex> Labex.Format.parse_func(:unknown)
+      ** (RuntimeError) Unknown type: unknown
+  """
+  def parse_func(type) do
+    case type do
+      :str -> & &1
+      :float -> &elem(Float.parse(&1), 0)
+      :int -> &String.to_integer(&1)
+      _ -> raise "Unknown type: #{type}"
+    end
   end
 end
--- test/format_test.exs
+++ test/format_test.exs
@@ -1,5 +1,16 @@
 defmodule FormatTest do
   use ExUnit.Case
+  use ExUnitProperties
 
   doctest Labex.Format
+
+  test "format/2" do
+    check all(
+            term1 <- one_of([boolean(), integer(), binary(), float(), atom(:alphanumeric)]),
+            term2 <- one_of([boolean(), integer(), binary(), float(), atom(:alphanumeric)])
+          ) do
+      assert "ab #{term1} cd #{term2} ef" ==
+               Labex.Format.format("ab {{term1}} cd {{term2}} ef", term1: term1, term2: term2)
+    end
+  end
 end
