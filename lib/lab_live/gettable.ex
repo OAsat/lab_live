41b986447e704bca26be19be085badd01d605b27
OAsat <o-asa@g.ecc.u-tokyo.ac.jp>
2023-10-24 15:45:54 +0900

    gettable

---
+++ lib/lab_live/gettable.ex
@@ -0,0 +1,54 @@
+defmodule LabLive.Gettable do
+  @moduledoc """
+  Store with getter function.
+
+  iex> {:ok, pid} = GenServer.start_link(LabLive.Gettable, fn -> 1 end)
+  iex> LabLive.Gettable.latest(pid)
+  nil
+  iex> LabLive.Gettable.get(pid)
+  1
+  iex> LabLive.Gettable.latest(pid)
+  1
+
+  iex> {:ok, pid} = GenServer.start_link(LabLive.Gettable, fn nil -> 1; value -> value + 1 end)
+  iex> LabLive.Gettable.get(pid)
+  1
+  iex> LabLive.Gettable.get(pid)
+  2
+  """
+  use GenServer
+
+  def start_link({name, getter}) do
+    GenServer.start_link(__MODULE__, getter, name: name)
+  end
+
+  @impl GenServer
+  def init(getter) do
+    {:ok, {getter, nil}}
+  end
+
+  @impl GenServer
+  def handle_call(:get, _from, {getter, _}) when is_function(getter, 0) do
+    value = getter.()
+    {:reply, value, {getter, value}}
+  end
+
+  @impl GenServer
+  def handle_call(:get, _from, {getter, latest}) when is_function(getter, 1) do
+    value = getter.(latest)
+    {:reply, value, {getter, value}}
+  end
+
+  @impl GenServer
+  def handle_call(:latest, _from, {getter, value}) do
+    {:reply, value, {getter, value}}
+  end
+
+  def get(pid) do
+    GenServer.call(pid, :get)
+  end
+
+  def latest(pid) do
+    GenServer.call(pid, :latest)
+  end
+end
---
+++ test/gettable_test.exs
@@ -0,0 +1,18 @@
+defmodule GettableTest do
+  alias LabLive.Gettable
+  use ExUnit.Case
+  use ExUnitProperties
+
+  doctest Gettable
+
+  test "getter/0" do
+    check all(
+            func_return <- term()
+          ) do
+      update_func = fn -> func_return end
+      {:ok, pid} = GenServer.start_link(Gettable, update_func)
+      assert func_return == Gettable.get(pid)
+      assert func_return == Gettable.latest(pid)
+    end
+  end
+end
