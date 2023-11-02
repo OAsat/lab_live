2432cd2a75109ac43880a80a8755be133e78e536
OAsat <o-asa@g.ecc.u-tokyo.ac.jp>
2023-11-02 13:44:44 +0900

    diagram

--- lib/lab_live/widgets.ex
+++ lib/lab_live/widgets.ex
@@ -1,5 +1,47 @@
 defmodule LabLive.Widgets do
+  use GenServer
   alias LabLive.Variables
+
+  def start_under_kino() do
+    Kino.start_child(__MODULE__)
+  end
+
+  def start_link(_opts) do
+    GenServer.start_link(__MODULE__, :ok, name: __MODULE__)
+  end
+
+  @impl GenServer
+  def init(:ok) do
+    diagram_frame = Kino.Frame.new()
+    {:ok, %{diagram_frame: diagram_frame}}
+  end
+
+  @impl GenServer
+  def handle_cast(:render_diagram, state) do
+    state.diagram_frame |> Kino.render()
+    {:noreply, state}
+  end
+
+  @impl GenServer
+  def handle_cast({:update_diagram, exec_state}, state) do
+    diagram = exec_state.diagram
+    status = exec_state.status
+
+    Kino.Frame.render(
+      state.diagram_frame,
+      LabLive.Diagram.to_mermaid_markdown(diagram, running: status)
+    )
+
+    {:noreply, state}
+  end
+
+  def update_diagram(exec_state) do
+    GenServer.cast(__MODULE__, {:update_diagram, exec_state})
+  end
+
+  def render_diagram() do
+    GenServer.cast(__MODULE__, :render_diagram)
+  end
 
   def render_props(props) do
     content =
