fd886db53ebaab4ca327b4d00edd131e325d607c
OAsat <o-asa@g.ecc.u-tokyo.ac.jp>
2023-11-05 23:53:59 +0900

    realtime plot

--- mix.exs
+++ mix.exs
@@ -24,6 +24,7 @@ defmodule LabLive.MixProject do
       {:timex, "~> 3.7"},
       {:telemetry, "~> 1.2"},
       {:kino, "~> 0.11.0"},
+      {:kino_vega_lite, "~> 0.1.10"},
       {:logger_file_backend, "~> 0.0.13"},
       {:ex_doc, "~> 0.30.8", only: :dev, runtime: false},
       {:stream_data, "~> 0.5", only: :test}
