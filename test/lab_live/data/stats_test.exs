c1ede36ebd6dcbcd054d6ac7f54a020beac4ce81
OAsat <o-asa@g.ecc.u-tokyo.ac.jp>
2023-11-11 05:28:57 +0900

    mod stats

--- test/lab_live/data/stats_test.exs
+++ test/lab_live/data/stats_test.exs
@@ -4,18 +4,35 @@ defmodule LabLive.Data.StatsTest do
   alias LabLive.Data.Stats
   doctest Stats
 
+  def from_list(list \\ [], max_size \\ :inf) when is_list(list) do
+    if max_size != :inf do
+      if length(list) > max_size do
+        raise "list size(#{length(list)}) must be same or less than max_size(#{max_size})."
+      end
+    end
+
+    %Stats{
+      size: length(list),
+      max_size: max_size,
+      sum: Enum.sum(list),
+      square_sum: Enum.sum(for x <- list, do: x * x),
+      queue: :queue.from_list(list)
+    }
+  end
+
   test "max_size = inf" do
     check all(values <- list_of(float(min: -1.0, max: 1.0))) do
       stats =
-        Enum.reduce(values, %Stats{}, fn value, stats ->
-          Stats.append(stats, value)
+        Enum.reduce(values, Stats.new(:inf), fn value, stats ->
+          Stats.update(stats, value)
         end)
 
-      from_list = Stats.new(values)
+      from_list = from_list(values)
 
       assert stats.max_size == :inf
       assert stats.size == from_list.size
       assert stats.queue |> :queue.to_list() == from_list.queue |> :queue.to_list()
+      assert Stats.value(stats) == List.last(values) || :empty
       assert_in_delta(stats.sum, from_list.sum, 1.0e-10)
       assert_in_delta(stats.square_sum, from_list.square_sum, 1.0e-10)
     end
@@ -27,8 +44,8 @@ defmodule LabLive.Data.StatsTest do
             values <- list_of(float(min: -1.0, max: 1.0))
           ) do
       stats =
-        Enum.reduce(values, Stats.new([], max_size), fn value, stats ->
-          Stats.append(stats, value)
+        Enum.reduce(values, Stats.new(max_size), fn value, stats ->
+          Stats.update(stats, value)
         end)
 
       valid_size =
@@ -38,7 +55,7 @@ defmodule LabLive.Data.StatsTest do
           length(values)
         end
 
-      from_list = Stats.new(values |> Enum.take(-valid_size))
+      from_list = from_list(values |> Enum.take(-valid_size))
 
       assert stats.max_size == max_size
       assert stats.size == valid_size
