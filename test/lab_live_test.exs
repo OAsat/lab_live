c1d08b71a95ff44b7375ac26fd38cc80f6da2902
OAsat <o-asa@g.ecc.u-tokyo.ac.jp>
2023-11-11 16:30:23 +0900

    render table data

--- test/lab_live_test.exs
+++ test/lab_live_test.exs
@@ -1,20 +1,60 @@
 defmodule LabLiveTest do
   use ExUnit.Case
+  use ExUnitProperties
   doctest LabLive
 
-  test "start_data/2" do
+  setup do
     start_supervised(
       {DynamicSupervisor, strategy: :one_for_one, name: LabLive.Data.TestSupervisor}
     )
 
+    :ok
+  end
+
+  test "start_data/2" do
     {:ok, pid} =
-      LabLive.start_data([init: 10, name: :test_start_data], LabLive.Data.TestSupervisor)
+      LabLive.start_data(:start_data_test, [init: 10], LabLive.Data.TestSupervisor)
 
     assert LabLive.Data.get(pid) == 10
+    assert LabLive.Data.get(:start_data_test) == 10
 
     {:override, ^pid} =
-      LabLive.start_data([init: 20, name: :test_start_data], LabLive.Data.TestSupervisor)
+      LabLive.start_data(:start_data_test, [init: 20], LabLive.Data.TestSupervisor)
 
     assert LabLive.Data.get(pid) == 20
+    assert LabLive.Data.get(:start_data_test) == 20
+  end
+
+  test "start_many_data/2" do
+    many_data = [
+      start_many_data_test1: [init: 10],
+      start_many_data_test2: [init: 20]
+    ]
+
+    [start_many_data_test1: {:ok, pid1}, start_many_data_test2: {:ok, pid2}] =
+      LabLive.start_many_data(many_data, LabLive.Data.TestSupervisor)
+
+    assert LabLive.Data.get(pid1) == 10
+    assert LabLive.Data.get(pid2) == 20
+  end
+
+  test "data_to_markdown/1" do
+    many_data = [
+      data_to_markdown_test1: [init: 10, label: "label1"],
+      data_to_markdown_test2: [init: 20, visible?: false],
+      data_to_markdown_test3: [init: 30],
+      data_to_markdown_test4: [init: LabLive.Data.Iterator.new([1, 2]), label: "label4"]
+    ]
+    LabLive.start_many_data(many_data, LabLive.Data.TestSupervisor)
+
+    expected = """
+    |key|label|value|
+    |--|--|--|
+    |data_to_markdown_test1|label1|10|
+    |data_to_markdown_test3||30|
+    |data_to_markdown_test4|label4|not_started <- [1, 2]|
+    """
+
+    assert expected == LabLive.data_to_markdown(many_data)
   end
 end
