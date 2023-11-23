6069eb519093f26901c9f80de8b1b75612de7153
OAsat <o-asa@g.ecc.u-tokyo.ac.jp>
2023-11-23 17:57:23 +0900

    dummy accepts joined query

--- test/lab_live/connection/method/dummy_test.exs
+++ test/lab_live/connection/method/dummy_test.exs
@@ -15,6 +15,7 @@ defmodule LabLive.Connection.Method.DummyTest do
     opts = [model: model]
     {:ok, pid} = start_supervised({Connection, method: Dummy, method_opts: opts})
     assert "dummy,1.0,1\n" == Connection.read(pid, "SAMPLE\n")
+    assert "dummy,1.0,1;dummy,1.0,1\n" == Connection.read(pid, "SAMPLE;SAMPLE\n")
   end
 
   test "reading a sample model" do
@@ -23,5 +24,13 @@ defmodule LabLive.Connection.Method.DummyTest do
     {:ok, pid} = start_supervised({Connection, method: Dummy, method_opts: opts})
     assert "1.0,1.0,1.0\r\n" == Connection.read(pid, "PID? 1\n")
     assert "1\r\n" == Connection.read(pid, "RANGE? 1\n")
+  end
+
+  test "reading a joined query" do
+    model = Lakeshore350.model()
+    opts = [model: model]
+    {:ok, pid} = start_supervised({Connection, method: Dummy, method_opts: opts})
+    assert "1.0,1.0,1.0;1.0,1.0,1.0\r\n" == Connection.read(pid, "PID? 1;PID? 1\n")
+    assert "1.0,1.0,1.0;1\r\n" == Connection.read(pid, "PID? 1;RANGE? 1\n")
   end
 end
