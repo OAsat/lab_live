2c5399e17680308b990aaab57b49421b07758023
OAsat <o-asa@g.ecc.u-tokyo.ac.jp>
2023-11-25 04:23:35 +0900

    smartcell starts instruments

--- lib/lab_live/widget/instruments_setup.ex
+++ lib/lab_live/widget/instruments_setup.ex
@@ -1,31 +1,45 @@
 defmodule LabLive.Widget.InstrumentsSetup do
   use Kino.JS, assets_path: "lib/assets/instruments_setup"
   use Kino.JS.Live
-  use Kino.SmartCell, name: "LabLive.Instruments"
-  # alias LabLive.Model
-  # alias LabLive.Model.Format
+  use Kino.SmartCell, name: "LabLive/Instruments"
+  alias LabLive.Model
 
   @impl true
   def init(attrs, ctx) do
     specs = attrs["specs"] || []
-    {:ok, assign(ctx, specs: specs)}
+    assign_to = attrs["assign_to"] || ""
+    {:ok, assign(ctx, specs: specs, assign_to: assign_to)}
   end
 
   @impl true
   def handle_connect(ctx) do
-    payload = %{specs: ctx.assigns.specs}
+    payload = %{specs: ctx.assigns.specs, assign_to: ctx.assigns.assign_to}
     {:ok, payload, ctx}
   end
 
   @impl true
   def to_attrs(ctx) do
-    %{"specs" => ctx.assigns.specs}
+    %{"specs" => ctx.assigns.specs, "assign_to" => ctx.assigns.assign_to}
   end
 
   @impl true
   def to_source(attrs) do
-    quote do
-      unquote(attrs["specs"])
+    case attrs["assign_to"] do
+      "" ->
+        quote do
+          unquote(attrs["specs"])
+          |> LabLive.Widget.InstrumentsSetup.convert_attrs_specs(__ENV__)
+          |> LabLive.Instrument.start_instruments()
+        end
+
+      assign_to ->
+        quote do
+          unquote({String.to_atom(assign_to), [], nil}) =
+            unquote(attrs["specs"])
+            |> LabLive.Widget.InstrumentsSetup.convert_attrs_specs(__ENV__)
+
+          LabLive.Instrument.start_instruments(unquote({String.to_atom(assign_to), [], nil}))
+        end
     end
     |> Kino.SmartCell.quoted_to_string()
   end
@@ -57,9 +71,85 @@ defmodule LabLive.Widget.InstrumentsSetup do
   end
 
   @impl true
-  def handle_event("form_changed", %{"specs" => specs}, ctx) do
-    ctx = assign(ctx, specs: specs)
+  def handle_event("form_changed", %{"specs" => specs, "assign_to" => assign_to}, ctx) do
+    ctx = assign(ctx, specs: specs, assign_to: assign_to)
     broadcast_event(ctx, "update_specs", specs)
     {:noreply, ctx}
+  end
+
+  def convert_attrs_specs(specs, env \\ []) do
+    for spec <- specs do
+      key = String.to_atom(spec["name"])
+      model = get_model(spec, env)
+
+      {key,
+       %{
+         name: key,
+         sleep_after_reply: get_sleep_after_reply(spec),
+         model: model,
+         selected_type: get_selected_type(spec),
+         dummy: get_dummy_spec(spec, model),
+         pyvisa: get_pyvisa_spec(spec),
+         tcp: get_tcp_spec(spec)
+       }}
+    end
+  end
+
+  defp parse_int_or_nil(str) do
+    case Integer.parse(str) do
+      {int, _} -> int
+      :error -> nil
+    end
+  end
+
+  defp get_sleep_after_reply(spec) do
+    parse_int_or_nil(spec["sleep_after_reply"])
+  end
+
+  defp get_model(spec, env) do
+    case spec["model"] do
+      "" ->
+        nil
+
+      exp ->
+        with {result, _} <- Code.eval_string(exp, [], env),
+             true <- is_map(result) do
+          Model.from_map(result)
+        else
+          _ -> nil
+        end
+    end
+  end
+
+  defp get_selected_type(spec) do
+    case spec["selected_type"] do
+      "Dummy" -> :dummy
+      "PyVISA" -> :pyvisa
+      "TCP/IP" -> :tcp
+    end
+  end
+
+  defp get_dummy_spec(spec, model) do
+    %{random: spec["dummy"]["if_random"] == "True", model: model}
+  end
+
+  defp get_pyvisa_spec(spec) do
+    %{address: spec["pyvisa"]["address"]}
+  end
+
+  defp get_tcp_spec(spec) do
+    %{port: parse_int_or_nil(spec["tcp"]["port"]), address: convert_tcp_address(spec["tcp"])}
+  end
+
+  defp convert_tcp_address(tcp_spec) do
+    address =
+      String.split(tcp_spec["address"], ".")
+      |> Enum.map(&parse_int_or_nil(&1))
+      |> Enum.reject(&is_nil(&1))
+
+    case length(address) do
+      4 -> address
+      _ -> nil
+    end
   end
 end
